355e1a9615b0b85144be833cb92273293cfea05f
sunney507 <sunney507@gmail.com>
2025-01-03 15:16:51 +0900

    fd

---
+++ Main.java
@@ -0,0 +1,27 @@
+  
+  public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 4, c = 3, d = 5;
+        if((a == 2 | a == c) & !(c > d) & (1 == b ^ c != d)) {
+
+            System.out.println(a == 2 | a == c);
+            System.out.println(!(c > d));
+            System.out.println(1 == b ^ c != d);
+            System.out.println();
+            System.out.println();
+         a = b + c;
+          if(7 == b ^ c != a) {
+           System.out.println(a);
+          } else {
+          System.out.println(b);
+          }
+        } else {
+          a = c + d;
+          if(7 == c ^ d != a) {
+          System.out.println(a);
+          } else {
+          System.out.println(d);
+          }
+        }
+    }
+  }
